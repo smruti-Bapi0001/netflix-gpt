08aa31fd6a68a2cdd00dd60c8d30f99e2bb764d2
Smruti Ranjan <sbapi0001@gmail.com>
2025-03-19 21:47:58 +0530

    bug fix

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,10 +4,13 @@ import VideoTitle from './VideoTitle';
 import VideoBackground from './VideoBackground';
 
 const MainContainer = () => {
-    const movies = useSelector(store=> store.movies?.nowPlayingMovies);
-    if(movies === null) return;
-    // const mainMovie = movies[0];
-    // console.log(mainMovie);
+    const movies = useSelector((store)=> store.movies?.nowPlayingMovies);
+
+    if(!movies) return;
+
+    const mainMovie = movies[0];
+    console.log(mainMovie);
+    // const {original_title, overview} = mainMovie;
   return (
     <div>
         <VideoTitle />
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,12 +1,10 @@
 import { API_OPTIONS } from '../utils/constants';
 import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies 
-    
-} from '../utils/moviesSlice';
+import { addNowPlayingMovies } from '../utils/moviesSlice';
 import { useEffect } from 'react';
 
 const useNowPlayingMovies = () => {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch();
 
   const nowPlayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
